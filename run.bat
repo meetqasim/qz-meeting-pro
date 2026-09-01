8f0d9a226b2d39855518f1faf0368758b75ae439
meetqasim <meetqasim@gmail.com>
2026-09-01 13:06:57 +0500

    Update server and client files

---
+++ run.bat
@@ -0,0 +1,3 @@
+git add main.js
+git commit -m "Fix Electron window file path"
+git push origin main
--- server.js
+++ server.js
@@ -1,24 +1,27 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, { cors: { origin: "*", methods: ["GET", "POST"] } });
+const io = new Server(server, {
+    cors: { origin: "*" }
+});
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', (socket) => {
-    socket.on('join-room', (roomId, username) => {
-        socket.join(roomId);
-        socket.to(roomId).emit('user-connected', { userId: socket.id, username });
+    socket.on('join-room', (room, username) => {
+        socket.join(room);
+        socket.to(room).emit('user-connected', { userId: socket.id, username });
 
-        socket.on('offer', (data) => io.to(data.target).emit('offer', { offer: data.offer, sender: socket.id, username: data.username }));
-        socket.on('answer', (data) => io.to(data.target).emit('answer', { answer: data.answer, sender: socket.id }));
-        socket.on('ice-candidate', (data) => io.to(data.target).emit('ice-candidate', { candidate: data.candidate, sender: socket.id }));
-        
+        socket.on('offer', data => io.to(data.target).emit('offer', { offer: data.offer, sender: socket.id, username }));
+        socket.on('answer', data => io.to(data.target).emit('answer', { answer: data.answer, sender: socket.id }));
+        socket.on('ice-candidate', data => io.to(data.target).emit('ice-candidate', { candidate: data.candidate, sender: socket.id }));
+
         socket.on('disconnect', () => {
-            socket.to(roomId).emit('user-disconnected', { userId: socket.id });
+            socket.to(room).emit('user-disconnected', { userId: socket.id });
         });
     });
 });
